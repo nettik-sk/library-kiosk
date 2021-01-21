781d2e6741dbe5710feb9b9f130222b935fc3a64
nettik-sk <75237785+nettik-sk@users.noreply.github.com>
2021-01-21 13:09:22 +0900

    Update KioskService.java

--- rental/src/main/java/library/external/KioskService.java
+++ rental/src/main/java/library/external/KioskService.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-//@FeignClient(name="kiosk", url="http://kiosk:8080")
-@FeignClient(name="kiosk", url="${api.kiosk.url}")
+@FeignClient(name="kiosk", url="http://kiosk:8080")
+//@FeignClient(name="kiosk", url="${api.kiosk.url}")
 public interface KioskService {
 
     @RequestMapping(method= RequestMethod.POST, path="/kiosks")
