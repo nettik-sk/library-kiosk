a97b5256bac9c59569a1d7341dbb987b8a251483
nettik-sk <75237785+nettik-sk@users.noreply.github.com>
2021-01-21 15:34:41 +0900

    Update Kiosk.java

--- kiosk/src/main/java/library/Kiosk.java
+++ kiosk/src/main/java/library/Kiosk.java
@@ -40,7 +40,13 @@ public class Kiosk {
             BeanUtils.copyProperties(this, selfReturned);
             selfReturned.publishAfterCommit();
         }
-
+        
+                //부하테스트 시간끌기
+        try {
+            Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
